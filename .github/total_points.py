cf4c478c76144ef83fbae2645ffed10fd1369c04
Antoine Kaufmann <antoinek@mpi-sws.org>
2024-06-02 22:44:50 +0200

    print points info to stdout as well

--- .github/total_points.py
+++ .github/total_points.py
@@ -10,9 +10,10 @@ def score_subtask(st, point_map):
     points = 0
     total = 0
     for k in point_map.keys():
-        if test_success(f'subtask1/test{k}.out'):
+        if test_success(f'{st}/test{k}.out'):
             points += point_map[k]
         total += point_map[k]
+    print(f'{st}: {points} of {total}')
     return (points, total)
 
 (p_1, max_1) = score_subtask('subtask1', {0: 1, 1: 3, 2: 1, 3: 3, 4: 2})
@@ -27,3 +28,5 @@ of = os.environ['GITHUB_OUTPUT']
 with open(of, 'a') as f:
     print(f'points={points}', file=f)
     print(f'max_points={m}', file=f)
+
+print(f'Total: {points} of {m}')
